32cd6cd922a275a508ccae12f85f862aad5f8bf3
lin37 <tinglei722@gmail.com>
2019-06-12 19:46:24 +0800

    测试MySQL用法

--- datagen/test.sql
+++ datagen/test.sql
@@ -13,7 +13,52 @@ customName,relName,relPhone,relEmail,relRelation)
 values
 ("425681199812347854","18369248907","siu67276",
 "Lin37","Sg","18378912678","syu@ssmk.com","mother");
-*/
+
 delete from customer where customID="425681199812347854";
 
-select * from customer;
+insert into customer
+(customID,customPhone,customAddress,
+customName,relName,relPhone,relEmail,relRelation)
+values
+("425681199812347854","18369248907","siu67276",
+"Lin37","Sg","18378912678","syu@ssmk.com","mother");
+
+insert into cus_and_cheAccount
+(cust_customID,cheq_cusA_accountIDX)
+values
+("425681199812347854","7900");
+
+insert into cusAccount
+(accountIDX,bran_branchName,
+
+
+insert into chequeAccount
+(cusA_accountIDX,neg_limit)
+values
+("7900",000087.00);
+
+select * from chequeAccount;
+select * from cus_and_cheAccount;
+
+
+insert into branch
+(city,branchName)
+values
+("win","union");
+
+insert into staff
+(staffID,bran_branchName)
+values
+("421381198712345678","union");
+*/
+
+insert into cusAccount
+(accountIDX,bran_branchName,staf_staffID,remain)
+values
+("1000","union","421381198712345678",3.56);
+
+select * from cusAccount;
+select * from staff;
+select * from branch;
+
+
