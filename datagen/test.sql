378919b84846f45cdc99adecacfcdecb17fa5024
lin37 <tinglei722@gmail.com>
2019-06-12 11:10:57 +0800

    测试MySQL

---
+++ datagen/test.sql
@@ -0,0 +1,19 @@
+use BankDB;
+/*
+insert into customer
+(customID,customPhone,customAddress,
+customName,relName,relPhone,relEmail,relRelation)
+values
+("421381199811117854","18369238907","siu76",
+"Lin","Song","18378905678","sqs@ssmk.com","father");
+
+insert into customer
+(customID,customPhone,customAddress,
+customName,relName,relPhone,relEmail,relRelation)
+values
+("425681199812347854","18369248907","siu67276",
+"Lin37","Sg","18378912678","syu@ssmk.com","mother");
+*/
+delete from customer where customID="425681199812347854";
+
+select * from customer;
